9461ef8ec4af2320c127ba35cc4f2ab2a4110354
Konstantin Kasterov <konstantin.kasterov@rosbank.ru>
2024-11-06 19:40:17 +0300

    lesson 2-3

---
+++ lesson2/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		word1 = "имя"
+		word2 = "твое"
+		word3 = "мне"
+		word4 = "знакомо"
+	)
+
+	fmt.Println(word4 + " " + word3 + " " + word2 + " " + word1)
+	fmt.Println(word3 + " " + word1 + " " + word4 + " " + word2)
+	fmt.Println(word2 + " " + word4 + " " + word1 + " " + word3)
+
+}
