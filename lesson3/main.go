82464e8793d91aafd83b6c940deeaf086d162d02
Konstantin Kasterov <konstantin.kasterov@rosbank.ru>
2024-11-07 21:31:20 +0300

    lesson 4

--- lesson3/main.go
+++ lesson3/main.go
@@ -6,12 +6,16 @@ import (
 
 func main() {
 	var (
-		first  int
-		second int
+		first  float32
+		second float32
+		total  int64
 	)
 
 	fmt.Scan(&first, &second)
-	var line int = (first + second) * (first + second)
-
-	fmt.Print(line)
+	total = int64(first + second)
+	if total%2.0 == 0 {
+		fmt.Println("ЧЁТНОЕ")
+	} else {
+		fmt.Println("НЕЧЁТНОЕ")
+	}
 }
