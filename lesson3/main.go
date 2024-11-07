a096c4a08e4cc9c4fac592ed8ea90b959ac20b55
Konstantin Kasterov <konstantin.kasterov@rosbank.ru>
2024-11-07 19:37:37 +0300

    lesson 2-3

--- lesson3/main.go
+++ lesson3/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	var (
+		first  int
+		second int
+	)
+
+	fmt.Scan(&first, &second)
+	var line int = (first + second) * (first + second)
+
+	fmt.Print(line)
 }
